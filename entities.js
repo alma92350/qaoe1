ea57d54c0dbd34d1007a12360b12dfa0804aa71d
alma92350 <almaatla@gmail.com>
2018-04-24 13:33:49 +0200

    Update entities.js

--- entities.js
+++ entities.js
@@ -2,7 +2,8 @@ var entities = [];
 
 entities.push({
   id: 22418,
-  interest: ['politics','science','ai','artificial intelligence','space','technology'],
+  interest: ['politics','science','ai','artificial intelligence','space','technology',
+            'oceanography'],
   subscriptions: ['technologyreview.com','sciencemag.org'],
   social:['twitter','linkedin'],
 });
